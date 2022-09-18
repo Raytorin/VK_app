b7bcb36f4dbc70629379d7cfce15f2b185da5e0b
Roman Pulyakho <pulromans@gmail.com>
2022-09-18 18:38:53 +0300

    Add Google Disk and fix some bug

--- main.py
+++ main.py
@@ -8,6 +8,45 @@ import requests
 
 import time
 
+from pydrive.auth import GoogleAuth
+
+from pydrive.drive import GoogleDrive
+
+import os
+
+class GoogleUploader:
+    def __init__(self, user_auth):
+        self.user_auth = user_auth
+
+    def upload(self, file_path: str):
+        print('Save photo to the cloud')
+        bar_upload = Bar('Processing', max=vk.vk_max_len, suffix='%(percent)d%%')
+        drive = GoogleDrive(self.user_auth)
+
+        folder_id = 'parent_folder_id'
+
+        file_metadata = {'title': f'{file_path}', 'parents': [folder_id], 'mimeType': 'application/vnd.google-apps.folder'}
+        folder_google = drive.CreateFile(file_metadata)
+        folder_google.Upload()
+
+        folders_all = drive.ListFile({'q': "title='" + f'{file_path}' + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+
+        for data_photo in vk.max_size():
+            correct_date = str(data_photo[2]).replace(':', '.')
+            filename_img = f'{data_photo[1]} likes, {correct_date}'
+            api = requests.get(data_photo[0])
+            content_img = api.content
+            with open('images/' + filename_img, 'wb') as f:
+                f.write(content_img)
+            for correct_folder in folders_all:
+                if correct_folder['title'] == file_path:
+                    upload_file = drive.CreateFile({'parents': [{'id': correct_folder['id']}]})
+                    upload_file.SetContentFile('images/' + filename_img)
+                    upload_file.Upload()
+                    bar_upload.next()
+            os.remove('images/' + filename_img)
+        return
+        
 
 class YaUploader:
     def __init__(self, token: str):
@@ -22,10 +61,10 @@ class YaUploader:
         params = {"path": disk_file_path, "overwrite": "true"}
         response = requests.put(create_url, headers=headers, params=params)
     
-    def _get_upload_link(self, disk_file_path):
+    def _get_upload_link(self, disk_file_path, folder):
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        params = {"path": 'images/%s' % disk_file_path, "overwrite": "true"}
+        params = {"path": f'{folder}/%s' % disk_file_path, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
         return response.json()
 
@@ -35,7 +74,7 @@ class YaUploader:
         self.create_folder(file_path)
         for data_photo in vk.max_size():
             correct_date = str(data_photo[2]).replace(':', '.')
-            response_link = self._get_upload_link(f'{data_photo[1]} likes, {correct_date}')
+            response_link = self._get_upload_link(f'{data_photo[1]} likes, {correct_date}', file_path)
             href = response_link.get("href", "")
             bar_upload.next()
             api = requests.get(data_photo[0])
@@ -102,9 +141,27 @@ if __name__ == '__main__':
 
     vk.max_size()
 
-    path_to_file = 'images'
-    token_ya = input('Введите Yandex токен: ')
-    uploader = YaUploader(token_ya)
-    result = uploader.upload(path_to_file)
-    print()
-    print('DONE')
+    path_to_file = 'images_from_vk'
+    while True:
+        print('\nКуда вы хотите загрузить фото?')
+        print('Выберите нужный варинт ответа')
+        print('1) Yandex disk \n2) Google disk\n')
+        user_choice = int(input('Ваш ответ: '))
+        if user_choice == 1:
+            token_ya = input('\nВведите Yandex токен: ')
+            uploader_ya = YaUploader(token_ya)
+            result = uploader_ya.upload(path_to_file)
+            print()
+            print('DONE')
+            break
+        elif user_choice == 2:
+            print('\nПройдите аунтефикацию')
+            gauth = GoogleAuth()
+            gauth.LocalWebserverAuth()
+            user_go = GoogleUploader(gauth)
+            result = user_go.upload(path_to_file)
+            print()
+            print('DONE')
+            break
+        else:
+            print(f'Команда {user_choice} не найдена, проверьте доступные команды')
